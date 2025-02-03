e282a3b38d583b5a7c8591d2f94c3f7da518c261
s1099 <46890315+s1099@users.noreply.github.com>
2025-02-04 00:10:22 +0530

    simple scene and player movement

---
+++ src/components/player.rs
@@ -0,0 +1,155 @@
+use std::f32::consts::FRAC_PI_2;
+
+use bevy::{
+    asset::Assets,
+    color::{palettes::tailwind, Color},
+    core_pipeline::core_3d::Camera3d,
+    ecs::{
+        component::Component,
+        query::With,
+        system::{Commands, Query, Res, ResMut, Single},
+    },
+    hierarchy::{BuildChildren, ChildBuild},
+    input::{keyboard::KeyCode, mouse::AccumulatedMouseMotion, ButtonInput},
+    math::{primitives::Cuboid, EulerRot, Quat, Vec2, Vec3},
+    pbr::{MeshMaterial3d, NotShadowCaster, StandardMaterial},
+    prelude::{Deref, DerefMut},
+    render::{
+        camera::{Camera, PerspectiveProjection, Projection},
+        mesh::{Mesh, Mesh3d},
+        view::{RenderLayers, Visibility},
+    },
+    time::Time,
+    transform::components::Transform,
+    utils::default,
+};
+
+#[derive(Debug, Component)]
+pub struct Player;
+
+#[derive(Debug, Component, Deref, DerefMut)]
+pub struct CameraSensitivity(Vec2);
+
+impl Default for CameraSensitivity {
+    fn default() -> Self {
+        Self(Vec2::new(0.003, 0.002))
+    }
+}
+
+#[derive(Debug, Component)]
+struct WorldModelCamera;
+
+#[derive(Debug, Component)]
+pub struct MovementSpeed(pub f32);
+
+pub fn spawn_player(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let arm = meshes.add(Cuboid::new(0.1, 0.1, 0.5));
+    let arm_material = materials.add(Color::from(tailwind::TEAL_200));
+
+    commands
+        .spawn((
+            Player,
+            CameraSensitivity::default(),
+            MovementSpeed(5.0),
+            Transform::from_xyz(0.0, 1.0, 0.0),
+            Visibility::default(),
+        ))
+        .with_children(|parent| {
+            parent.spawn((
+                WorldModelCamera,
+                Camera3d::default(),
+                Projection::from(PerspectiveProjection {
+                    fov: 90.0_f32.to_radians(),
+                    ..default()
+                }),
+            ));
+
+            parent.spawn((
+                Camera3d::default(),
+                Camera {
+                    order: 1,
+                    ..default()
+                },
+                Projection::from(PerspectiveProjection {
+                    fov: 70.0_f32.to_radians(),
+                    ..default()
+                }),
+                RenderLayers::layer(1),
+            ));
+
+            // right arm
+            parent.spawn((
+                Mesh3d(arm),
+                MeshMaterial3d(arm_material),
+                Transform::from_xyz(0.2, -0.1, -0.25),
+                // Ensure the arm is only rendered by the view model camera.
+                RenderLayers::layer(1),
+                // The arm is free-floating, so shadows would look weird.
+                NotShadowCaster,
+            ));
+        });
+}
+
+pub fn move_player(
+    accumulated_mouse_motion: Res<AccumulatedMouseMotion>,
+    player: Single<(&mut Transform, &CameraSensitivity), With<Player>>,
+) {
+    let (mut transform, camera_sensitivity) = player.into_inner();
+
+    let delta = accumulated_mouse_motion.delta;
+
+    if delta != Vec2::ZERO {
+        let delta_yaw = -delta.x * camera_sensitivity.x;
+        let delta_pitch = -delta.y * camera_sensitivity.y;
+
+        let (yaw, pitch, roll) = transform.rotation.to_euler(EulerRot::YXZ);
+        let yaw = yaw + delta_yaw;
+
+        const PITCH_LIMIT: f32 = FRAC_PI_2 - 0.01;
+        let pitch = (pitch + delta_pitch).clamp(-PITCH_LIMIT, PITCH_LIMIT);
+
+        transform.rotation = Quat::from_euler(EulerRot::YXZ, yaw, pitch, roll);
+    }
+}
+
+pub fn player_movement(
+    time: Res<Time>,
+    keys: Res<ButtonInput<KeyCode>>,
+    mut player: Query<(&mut Transform, &MovementSpeed), With<Player>>,
+) {
+    let Ok((mut transform, speed)) = player.get_single_mut() else {
+        return;
+    };
+
+    // forward and right directions projected onto the XZ plane
+    let forward = transform.forward();
+    let horizontal_forward = Vec3::new(forward.x, 0.0, forward.z).normalize_or_zero();
+
+    let right = transform.right();
+    let horizontal_right = Vec3::new(right.x, 0.0, right.z).normalize_or_zero();
+
+    let mut direction = Vec3::ZERO;
+
+    if keys.pressed(KeyCode::KeyW) {
+        direction += horizontal_forward;
+    }
+    if keys.pressed(KeyCode::KeyS) {
+        direction -= horizontal_forward;
+    }
+    if keys.pressed(KeyCode::KeyA) {
+        direction -= horizontal_right;
+    }
+    if keys.pressed(KeyCode::KeyD) {
+        direction += horizontal_right;
+    }
+
+    if direction.length_squared() > 0.0 {
+        direction = direction.normalize();
+    }
+
+    transform.translation += direction * speed.0 * time.delta_secs();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,71 @@
+mod components;
+use bevy::app::Update;
+use bevy::color::palettes::tailwind;
+use bevy::color::Color;
+use bevy::pbr::PointLight;
+use bevy::prelude::Startup;
+use bevy::render::view::RenderLayers;
+use bevy::transform::components::Transform;
+use bevy::{
+    app::App,
+    asset::{AssetServer, Assets},
+    color::Srgba,
+    ecs::system::{Commands, Res, ResMut},
+    math::primitives::{Cuboid, Plane3d},
+    pbr::{MeshMaterial3d, StandardMaterial},
+    render::mesh::{Mesh, Mesh3d, Meshable},
+    utils::default,
+    DefaultPlugins,
+};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, (setup, components::player::spawn_player))
+        .add_systems(
+            Update,
+            (
+                components::player::move_player,
+                components::player::player_movement,
+            ),
+        )
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    asset_server: Res<AssetServer>,
+) {
+    commands.spawn((
+        Mesh3d(meshes.add(Plane3d::default().mesh().size(20.0, 20.0))),
+        MeshMaterial3d(materials.add(StandardMaterial {
+            base_color: Srgba::hex("#e1ed5f").unwrap().into(),
+            ..default()
+        })),
+    ));
+
+    for x in -2..3 {
+        for z in -2..3 {
+            commands.spawn((
+                Mesh3d(meshes.add(Cuboid::default())),
+                MeshMaterial3d(materials.add(StandardMaterial {
+                    base_color: Srgba::hex("#61cbe8").unwrap().into(),
+                    ..default()
+                })),
+                Transform::from_xyz(x as f32 * 2.0, 0.5, z as f32 * 2.0),
+            ));
+        }
+    }
+
+    commands.spawn((
+        PointLight {
+            color: Color::from(tailwind::ROSE_300),
+            shadows_enabled: true,
+            ..default()
+        },
+        Transform::from_xyz(-2.0, 5.0, -0.75),
+        RenderLayers::from_layers(&[0, 1]),
+    ));
 }
